3e18744b6605b0638d9cc4aea2175331cf220bbb
scott198989 <youremail@example.comscott-tuschl@comcast.net>
2023-06-11 15:14:14 -0500

    front end assesment is 100%complete, attempted backend and was unsure of directions. Final commit

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -3,6 +3,10 @@ import { Button, Input, Label } from 'reactstrap';
 import ModalComponent from './components/ModalComponent';
 import './App.css';
 
+//Psuedocode: Above are the necessary dependencies imported. The main function living on this page, App, has two sets of state being utilized.  The first set of state deals with the Modal and it is set to false so that the only time the modal will appear is when an event listener is triggered, which would be the click of a button.  The second set of state deals with the user name, which is set to an empty object with a property called name allowing the user to input a name.  The handleChange is a event listener that is waiting for interaction with the user, when the user modifies this area the function is called, i.e. entering in a name.  inside the function I used the spread operator to copy the username object.  When the user enters a name, the object is updated to reflect it.  In a nutshell, when you enter a name in the input field the userName object stores that name and passes it to the modal component for the custom greeting.  The handleClickModal is set to true so that the modal component (ModalComponent) will be displayed because the isOpen prop of the modal component is connected to the prepModal state. When prepModal is true, the modal is shown; when prepModal is false, the modal is hidden.  The handleReset is set to false so that it only resets when the user clicks the button, and once that is triggered, it will set the object that currently holds the users name to be a empty obejct again.  I also did some inline styling to set the color of the buttons to #0dcaf0 and the text to white because for some reason i was not able to do that in css.  I will look into it, I should have been able to, it might be how I have my class set up in JSX.  
+
+
+
 const App = () => {
   const [prepModal, setPrepModal] = useState(false);
   const [userName, setUserName] = useState({
@@ -24,16 +28,19 @@ const App = () => {
 
   return (
     <div className="entire-content">
-      <h1>Preparedness Assessment</h1>
+      <h1 className="header">Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
           <Label for="name">Enter your name</Label>
           <Input type="text" name="name" onChange={handleChange} value={userName.name} />
         </div>
-        <Button onClick={handleClickModal}>Click Me</Button>
-        <Button onClick={handleReset}>Reset</Button>
-        <ModalComponent isOpen={prepModal} toggle={() => setPrepModal(!prepModal)} userName={userName.name} />
-      </div>
+        <div className="btns">
+  <button onClick={handleClickModal} style={{ backgroundColor: '#0dcaf0', color: 'white' }}>Click Me</button>
+  <button onClick={handleReset} style={{ backgroundColor: '#0dcaf0', color: 'white' }}>Reset</button>
+  <ModalComponent isOpen={prepModal} toggle={() => setPrepModal(!prepModal)} userName={userName.name} />
+</div>
+
+    </div>
     </div>
   );
 };
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'; // standard import for react component 
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+//Psuedocode: Using reactstrap, to import the specific modal requested this is the import needed, and it comes from reactstrap.The two props in this function (isOpen, toggle) are what is allowing the modal to appear. The isOpen prop controls the visability of the modal, while toggle shuts the modal down when the user clicks outside the modal, it updates the state of the parent component, and the userName is to display the customer greeting when the modal appears. The lorem ispom is wrapped in the ModalBody tag to act as its container.  
 
-const ModalComponent = ({ isOpen, toggle, userName }) => {
+const ModalComponent = ({ isOpen, toggle, userName }) => { 
   return (
     <div>
-      <Modal isOpen={isOpen} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Hello, {userName}</ModalHeader>
+      <Modal isOpen={isOpen} toggle={toggle}> 
+        <ModalHeader toggle={toggle}>Hello, {userName}!</ModalHeader>
         <ModalBody>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -13,14 +14,6 @@ const ModalComponent = ({ isOpen, toggle, userName }) => {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
           est laborum.
         </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Do Something
-          </Button>{' '}
-          <Button color="secondary" onClick={toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
       </Modal>
     </div>
   );
